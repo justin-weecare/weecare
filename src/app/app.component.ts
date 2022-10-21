dd1c00e8ed1f2ceadf9d9074bf5213e7655537d5
Justin Tung <justin@Justins-MacBook-Pro-2.local>
2022-10-21 07:52:57 -0700

    fetch data and style albums

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,29 @@ export class AppComponent implements OnInit {
   albums: Album[] = [];
   filteredAlbums: Album[] = [];
 
-  ngOnInit(): void {
-    
+  async ngOnInit(): Promise<void> {
+    await this.fetchData();
+  }
+
+  async fetchData() {
+    try {
+      let response = await fetch('https://itunes.apple.com/us/rss/topalbums/limit=100/json');
+      if (response.ok) {
+        this.albums = (await response.json() as any).feed.entry.map((album: any) => {
+          return {
+            title: album['im:name'].label,
+            imageUrl: album['im:image'][album['im:image'].length - 1].label,
+            artist: album['im:artist'].label,
+            price: album['im:price'].label,
+            itemCount: album['im:itemCount'].label,
+            rights: album.rights.label,
+            releaseDate: album['im:releaseDate'].label,
+          };
+        });
+        this.filteredAlbums = this.albums;
+      }
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
