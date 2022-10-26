65da9173fac7a1ba61a49f659a2f499182bd264c
Justin Tung <justin@Justins-MacBook-Pro-2.local>
2022-10-26 14:06:12 -0700

    fix modal scroll issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { debounce } from 'lodash';
 
 import { Album } from './components/album/album.types';
 import { AlbumService } from './services/album.service';
+import { ModalService } from './services/modal.service';
 
 @Component({
   selector: 'app-root',
@@ -18,17 +19,20 @@ export class AppComponent implements OnInit {
     const searchString = this.searchString.trim().toLowerCase();
     this.filteredAlbums = this.albums.filter(album => album.title.toLowerCase().includes(searchString) || album.artist.toLowerCase().includes(searchString));
   }, 200);
-  isModalVisible = false;
 
-  constructor(private albumService: AlbumService) { }
+  constructor(private albumService: AlbumService, private modalService: ModalService) { }
 
   async ngOnInit(): Promise<void> {
     this.albums = await this.albumService.getAlbums();
     this.filteredAlbums = this.albums;
   }
 
+  get isModalVisible(): boolean {
+    return Object.values(this.modalService.modals).some(modal => modal['visible']);
+  }
+
   onClickAlbum(album: Album) {
     this.selectedAlbum = album;
-    this.isModalVisible = true;
+    this.modalService.setVisibility('albumDetail', true);
   }
 }
